1174806b30c210a6095c165c370f652d8d9f79e0
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-17 07:41:16 -0300

    refact stream lambda

--- src/main/java/com/devs/feedback_servless/handlers/StreamLambdaHandler.java
+++ src/main/java/com/devs/feedback_servless/handlers/StreamLambdaHandler.java
@@ -1,31 +1,19 @@
 package com.devs.feedback_servless.handlers;
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.devs.feedback_servless.FeedbackServlessApplication;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public class StreamLambdaHandler implements RequestStreamHandler {
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(FeedbackServlessApplication.class);
-        } catch (ContainerInitializationException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
+public class StreamLambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
-        handler.proxyStream(inputStream, outputStream, context);
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent request,
+            Context context) {
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withBody("OK");
     }
 }
