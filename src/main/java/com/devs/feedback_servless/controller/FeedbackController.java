0c3a00bd4a37a9f7f07748640e99cf20e247c817
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-17 07:58:46 -0300

    reafct controller

--- src/main/java/com/devs/feedback_servless/controller/FeedbackController.java
+++ src/main/java/com/devs/feedback_servless/controller/FeedbackController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/feedback")
+@RequestMapping("/avaliacao")
 public class FeedbackController {
 
     private final FeedbackService service;
