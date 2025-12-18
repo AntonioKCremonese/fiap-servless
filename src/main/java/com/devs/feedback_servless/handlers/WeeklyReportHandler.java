ee6d7ddeba468b8d2ab3a7b53d63fd172b18109a
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-17 22:21:32 -0300

    ajuste WeeklyReportHandler

--- src/main/java/com/devs/feedback_servless/handlers/WeeklyReportHandler.java
+++ src/main/java/com/devs/feedback_servless/handlers/WeeklyReportHandler.java
@@ -3,22 +3,24 @@ package com.devs.feedback_servless.handlers;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import com.devs.feedback_servless.config.AwsClientConfig;
 import com.devs.feedback_servless.service.ReportService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class WeeklyReportHandler implements RequestHandler<ScheduledEvent, String> {
 
-    private static final AnnotationConfigApplicationContext context;
-    private static final ReportService service;
-
-    static {
-        context = new AnnotationConfigApplicationContext("com.devs.feedback_servless");
-        service = context.getBean(ReportService.class);
-    }
+    private static AnnotationConfigApplicationContext context;
+    private static ReportService reportService;
 
     @Override
     public String handleRequest(ScheduledEvent event, Context ctx) {
-        service.generateAndStoreWeeklyReport();
+
+        if (context == null) {
+            context = new AnnotationConfigApplicationContext(AwsClientConfig.class);
+            reportService = context.getBean(ReportService.class);
+        }
+
+        reportService.generateAndStoreWeeklyReport();
         return "REPORT_OK";
     }
 }
