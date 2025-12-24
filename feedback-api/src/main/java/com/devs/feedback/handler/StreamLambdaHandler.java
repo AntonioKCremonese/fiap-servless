26150adca3468fe16ebd2112bc6b962f56817ce7
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-23 22:52:23 -0300

    refact template.yaml

--- feedback-api/src/main/java/com/devs/feedback/handler/StreamLambdaHandler.java
+++ feedback-api/src/main/java/com/devs/feedback/handler/StreamLambdaHandler.java
@@ -8,23 +8,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.devs.feedback.ApiApplication;
 
-//public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
-//
-//    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-//
-//    static {
-//        try {
-//            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(
-//                    ApiApplication.class
-//            );
-//        } catch (ContainerInitializationException e) {
-//            throw new RuntimeException("Erro ao iniciar Spring", e);
-//        }
-//    }
-//
-//    @Override
-//    public AwsProxyResponse handleRequest(AwsProxyRequest request, Context context) {
-//        return handler.proxy(request, context);
-//    }
-//
-//}
+public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(
+                    ApiApplication.class
+            );
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Erro ao iniciar Spring", e);
+        }
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest request, Context context) {
+        return handler.proxy(request, context);
+    }
+
+}
