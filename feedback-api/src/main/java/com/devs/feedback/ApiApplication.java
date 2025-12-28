1e70338a5d8d6728647a993c9153703a5a71b5b0
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-28 08:09:26 -0300

    alterad api application

--- feedback-api/src/main/java/com/devs/feedback/ApiApplication.java
+++ feedback-api/src/main/java/com/devs/feedback/ApiApplication.java
@@ -1,14 +1,14 @@
 package com.devs.feedback;
+import com.devs.feedback.controller.FeedbackController;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "com.devs.feedback")
+@SpringBootApplication
+@Import({FeedbackController.class})
 public class ApiApplication {
+
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ApiApplication.class)
-                .web(WebApplicationType.NONE)
-                .run(args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
