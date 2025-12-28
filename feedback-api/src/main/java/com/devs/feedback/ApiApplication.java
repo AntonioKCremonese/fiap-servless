e528ec71884de8993c2303f8af2209798fb7a201
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-12-28 11:30:05 -0300

    ajuste api

--- feedback-api/src/main/java/com/devs/feedback/ApiApplication.java
+++ feedback-api/src/main/java/com/devs/feedback/ApiApplication.java
@@ -1,12 +1,15 @@
 package com.devs.feedback;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication app = new SpringApplication(ApiApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
     }
 }
